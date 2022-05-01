30039af2c5ae61084dbec187056f47527ae540dc
Devy <67055084+Yeo-injun@users.noreply.github.com>
2022-05-02 01:20:18 +0900

    axios공통 설정 별도 모듈로 추출 - defaultAxios

--- src/api/MyTeamAPI.js
+++ src/api/MyTeamAPI.js
@@ -1,26 +1,11 @@
-import axios from 'axios';
+import axios from './defaultAxios';
+import { SERVER_URL } from './const';
 
-// API설정 공통화
-// 참고자료 : https://jess2.xyz/vue/vue-tip/#2-router--%EC%BB%B4%ED%8F%AC%EB%84%8C%ED%8A%B8-%EC%84%A4%EA%B3%84
-const axiosService = axios.create({
-    // baseURL: 'http://54.180.147.129/api/myTeams'
-    baseURL:'http://localhost:8080/api/myTeams',
-    headers:{
-        "Content-Type": "application/json"
-    }
-})
+const axiosService = axios.createDefault('/myTeams', SERVER_URL.LOCAL);
 
-<<<<<<< HEAD
 // 객체로 api모듈을 관리한다.
 export const myTeamApi = {
     getMyTeams(data) {
         return axiosService.get(`/${data.userSeq}`); 
     },
 }
-=======
-export const tempApi = {
-    getMyTeams (data) {
-        return axiosService.get(`/${data.userSeq}`);
-    }
-}
->>>>>>> 73ffae477db2804d841880c36724b2317adabdee
