989046f6f74b8df6368c6e657f5636dac99c06b0
강창기 <elien940318@gmail.com>
2022-05-02 22:36:23 +0900

    여백 조정

--- src/components/myTeam/MyProfile.vue
+++ src/components/myTeam/MyProfile.vue
@@ -3,13 +3,12 @@
         <v-chip label color="green" text-color="white">개인프로필</v-chip>
         
         <v-container>
-            <v-card class="grey lighten-2">
+            <v-card class="grey lighten-2 pa-3">
                 <v-row align="center">
-                    <v-col class="pa-3" cols="2">
+                    <v-col cols="2">
                         <v-img max-height="100" max-width="100" src="https://scontent.ficn1-1.fna.fbcdn.net/v/t1.6435-9/42930628_2123047524380299_6256315174584582144_n.png?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=ShQS0dwV8PEAX9DlceS&_nc_ht=scontent.ficn1-1.fna&oh=00_AT-imSTO-8K8Fg4AZ2UTDAer242lnfa32yQqt67OT4V1jg&oe=628E2C34"/>
                     </v-col>
-                    <v-spacer/>
-                    <v-col class="pa-3" cols="10">
+                    <v-col cols="10">
                         <v-row justify="space-between">
                             <v-col><v-card>포지션: </v-card></v-col>
                             <v-col><v-card>직책: </v-card></v-col>
--- src/views/myTeam/MyTeamPage.vue
+++ src/views/myTeam/MyTeamPage.vue
@@ -6,15 +6,22 @@
                 <v-tab to="/myTeam/members">팀원 조회</v-tab>
                 <v-tab to="/myTeams/test">팀기록 조회</v-tab>
             </v-tabs>
-
             <router-view/>
         </v-container>
     </div>
 </template>
 
 <script>
+    //import { API } from '@/api/MyTeamAPI.js';
     export default {
-        
+        methods: {
+            initTab() {
+                
+            }
+        },
+        mounted: {
+            //this.initTab();
+        }
     }
 </script>
 
