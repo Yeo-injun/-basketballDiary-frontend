5eb921c63d7ae0515a0761170e60b6cb4abe17a8
seongju lee <gch03915@naver.com>
2022-05-13 23:39:32 +0900

    내정보 데이터 긁어옴

--- src/components/loginUser/MyProfile.vue
+++ src/components/loginUser/MyProfile.vue
@@ -5,27 +5,37 @@
         <v-card>
             <v-row>
                 <v-col>이름 :</v-col>
-                <v-col></v-col>
+                <v-col>{{myinfo.userName}}</v-col>
             </v-row>
             <v-row>
                 <v-col>이메일 :</v-col>
-                <v-col></v-col>
+                <v-col>{{myinfo.email}}</v-col>
             </v-row>
             <v-row>
                 <v-col>포지션 : </v-col>
-                <v-col></v-col>
+                <v-col>
+                    <v-row>
+                        <v-checkbox v-model="selected" label="포인트가드" value="포인트가드"></v-checkbox>
+                        <v-checkbox v-model="selected" label="슈팅가드" value="슈팅가드"></v-checkbox>                
+                        <v-checkbox v-model="selected" label="스몰포워드" value="스몰포워드"></v-checkbox>                
+                        <v-checkbox v-model="selected" label="파워포워드" value="파워포워드"></v-checkbox>                
+                        <v-checkbox v-model="selected" label="센터" value="센터"></v-checkbox>
+                    </v-row>                 
+                </v-col>                
             </v-row>
             <v-row>
                 <v-col>키 : </v-col>
-                <v-col></v-col>
+                <v-col>{{myinfo.height}}</v-col>
             </v-row>
             <v-row>
                 <v-col>몸무게 : </v-col>
-                <v-col></v-col>
+                <v-col>{{myinfo.weight}}</v-col>
             </v-row>
             <v-row>
                 <v-col>활동지역 : </v-col>
-                <v-col></v-col>
+                <v-col>
+                    <v-select :items="items" label="시도" solo></v-select>
+                </v-col>
             </v-row>
         </v-card>
         <v-btn class="float-right">수정</v-btn>
@@ -35,8 +45,28 @@
 </template>
 
 <script>
-export default {
+import myProfileApi from '@/api/AuthUserAPI';
 
+export default {
+    data: () => {
+        return {
+            myinfo:{},
+            selected:[],
+            items: ['시도','서울시','부산시','대구시','대전시','광주시','울산시','세종시','인천시','경기도','강원도','충청북도','충청남도','경상북도','경상남도','전라북도','전라남도','제주도'],
+        }
+    },
+    methods: {
+        async load(){
+            const params = {//eslint-disable-line no-unused-vars
+                userSeq: 3,
+            };
+            this.myinfo = (await myProfileApi.getMyInfo()).data;
+            console.log(this.myinfo.data);
+        }
+    },
+    mounted () {
+        this.load();
+    }
 }
 </script>
 
