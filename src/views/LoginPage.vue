4c2c88320f02175fb92e11afd0a504afef7cf6f1
Devy <67055084+Yeo-injun@users.noreply.github.com>
2022-05-22 23:41:09 +0900

    [FIX] 프로미스 방식의 .then() .catch() 코딩 지양. customAxios에 공통 에러 처리 기능 추가 예정

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -22,24 +22,24 @@
             // 비동기 처리
             async doLogin()
             {
-                const param = 
-                {
+                const param = {
                     userId: this.userId,
                     password: this.password
                 }
-                await userApi.login(param)
-                        .then((res) => {
-                            console.log(res.headers);
-                            // 참고자료 : https://v3.router.vuejs.org/guide/essentials/navigation.html#router-push-location-oncomplete-onabort
-                            // 인증을 위한 쿠키 사용에 대한 추가 설정 필요 여부 확인 필요 - store 확인 https://developerjournal.tistory.com/15 (중앙 상태관리 libarary vuex)
-                            // 'login-success'라는 이름의 커스텀 이벤트를 res 데이터와 함께 상위 컴포넌트로 올려보냄
-                            this.$emit('login-success', res); // loginSuccess라는 이름의 이벤트를 발생시켜서 상위 컴포넌트로 올린다.
-                        })
-                        .catch((e) => {
-                            // 함수로 감싸기 - 오류코드에 따라 에러 메세지 처리
-                            alert(e.response.data.message);
-                            this.initInput();
-                        })
+                try {
+                    /**
+                     * 참고자료 : https://v3.router.vuejs.org/guide/essentials/navigation.html#router-push-location-oncomplete-onabort
+                     * 인증을 위한 쿠키 사용에 대한 추가 설정 필요 여부 확인 필요 - store 확인 https://developerjournal.tistory.com/15 (중앙 상태관리 libarary vuex)
+                     * 'login-success'라는 이름의 커스텀 이벤트를 res 데이터와 함께 상위 컴포넌트로 올려보냄
+                     */
+                    const res = await userApi.login(param);
+                    console.log(res.headers);
+                    this.$emit('login-success', res);
+                } catch(e) {
+                    // 함수로 감싸기 - 오류코드에 따라 에러 메세지 처리
+                    alert(e.response.data.message);
+                    this.initInput();
+                }
             },
             initInput() 
             {
