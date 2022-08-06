58a197f9615d156717edfc8b95dd8ebb30a30ca7
Devy <67055084+Yeo-injun@users.noreply.github.com>
2022-08-06 22:13:49 +0900

    [Feat] 소속팀 팀원조회 화면 - 소속팀의 권한에 따라 버튼 display상태 제어, authUtil.js추가

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <!-- https://vuetifyjs.com/en/components/application/#default-application-markup -->
   <!-- v-app태그가 vuetify의 기본 골격됨. 도화지 역할 -->
   <v-app>
-    <AppNavbar :is-login="isLogin" @logout-success="deleteAuthUserInfo" />
+    <AppNavbar :p-is-login="isLogin" @logout-success="deleteAuthUserInfo" />
     <!-- 라우팅을 이용해서 v-main 컴포넌트 하위의 내용 바꿔치기 하기 -->
     <v-main>
       <!-- v-main 하위의 컴포넌트는 라우팅에 따라서 다른 컴포넌트 보여주기 : Tap 영역 컴포넌트 추가 -->
--- src/components/myTeam/MyTeamMemberTab.vue
+++ src/components/myTeam/MyTeamMemberTab.vue
@@ -1,145 +1,167 @@
 <template>
-    <div>
-        <v-container class="px-15">
-            <div class="d-flex">
-                <v-subheader>개인프로필</v-subheader>
+  <div>
+    <v-container class="px-15">
+      <div class="d-flex">
+        <v-subheader>개인프로필</v-subheader>
 
-                <v-btn color="black white--text" small 
-                @click.stop="teamProfile=true">
-                    프로필 수정
-                </v-btn>
-                <MyTeamProfileModal
-                :teamSeq="pTeamSeq" 
-                :value="teamProfile" 
-                @input="teamProfile = $event"/>
+        <v-btn color="black white--text" small @click.stop="teamProfile = true">
+          프로필 수정
+        </v-btn>
+        <MyTeamProfileModal
+          :teamSeq="pTeamSeq"
+          :value="teamProfile"
+          @input="teamProfile = $event"
+        />
 
-                <v-btn color="black white--text" small 
-                @click.stop="dialog=true">
-                    팀정보 수정
-                </v-btn>
-                <MyTeamInfoModal 
-                v-model="dialog" 
-                @input="dialog=$event" 
-                :pTeamSeq="pTeamSeq"/>
-            </div>
-            <MyProfile :data="profile" />
+        <v-btn
+          v-if="this.isManager()"
+          color="black white--text"
+          small
+          @click.stop="dialog = true"
+        >
+          팀정보 수정
+        </v-btn>
+        <MyTeamInfoModal
+          v-model="dialog"
+          @input="dialog = $event"
+          :pTeamSeq="pTeamSeq"
+        />
+      </div>
+      <MyProfile :data="profile" />
 
-            <v-subheader>운영진</v-subheader>
-            <div v-for="(manager, index) in managerList" v-bind:key="index">
-                <MyManager v-bind:data="manager" />
-            </div>
+      <v-subheader>운영진</v-subheader>
+      <div v-for="(manager, index) in managerList" v-bind:key="index">
+        <MyManager :data="manager" :pTeamSeq="pTeamSeq" />
+      </div>
 
-            <div class="d-flex">
-                <v-subheader>팀원 목록</v-subheader>
-                <v-btn @click="clickAddTeamMember" class="ml-auto" color="black white--text" small>팀원 추가</v-btn>
-            </div>
-            <div v-for="(member, index) in memberList" v-bind:key="'A'+index">
-                <MyMember v-bind:data="member" />
-            </div>
-            <div class="text-center">
-                <v-pagination 
-                v-model="pagination.page" 
-                :length="pagination.totPagerNo"
-                @input="handlePage" />
-            </div>
-        </v-container>
-    </div>
+      <div class="d-flex">
+        <v-subheader>팀원 목록</v-subheader>
+        <v-btn
+          v-if="this.isManager()"
+          @click="clickAddTeamMember"
+          class="ml-auto"
+          small
+          color="black white--text"
+        >
+          팀원 추가
+        </v-btn>
+      </div>
+      <div v-for="(member, index) in memberList" v-bind:key="'A' + index">
+        <MyMember :data="member" :pTeamSeq="pTeamSeq" />
+      </div>
+      <div class="text-center">
+        <v-pagination
+          v-model="pagination.page"
+          :length="pagination.totPagerNo"
+          @input="handlePage"
+        />
+      </div>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import myTeamApi from '@/api/MyTeamAPI';
-import MyProfile from '@/components/myTeam/MyProfile.vue';
-import MyManager from '@/components/myTeam/MyManager.vue';
-import MyMember from '@/components/myTeam/MyMember.vue';
-import MyTeamInfoModal from '@/components/myTeam/modal/MyTeamInfoModal.vue'; 
+import authUtil from "@/common/AuthUtil.js";
+
+import myTeamApi from "@/api/MyTeamAPI";
+import MyProfile from "@/components/myTeam/MyProfile.vue";
+import MyManager from "@/components/myTeam/MyManager.vue";
+import MyMember from "@/components/myTeam/MyMember.vue";
+import MyTeamInfoModal from "@/components/myTeam/modal/MyTeamInfoModal.vue";
 // TODO 컴포넌트 폴더로 옮기기
-import MyTeamProfileModal from '@/views/myTeam/modal/MyTeamProfileModal.vue';
-    
-    export default {
-        //data: {} // Component끼리 data를 공유하면 안되므로 다음과 같이 사용하면 안됨.
-        data: () => {
-            return {
-                profile: {},
-                managerList: [],
-                memberList: [],
-                teamInfo: {},
-                dialog: false,
-                teamProfile: false,
-                pagination : {
-                    page: 1,
-                    teamCount: 0,
-                    totPagerNo: 1,
-                }
-            }
-        },
-        props: {
-            pTeamSeq: {
-                type: Number,
-                required: true
-            }
-        },
-        components: {
-            MyProfile,
-            MyManager,
-            MyMember,
-            // eslint-disable-next-line
-            MyTeamInfoModal,
-            MyTeamProfileModal
-        },
-        methods: {
-            onLoad() {
-                this.getProflie();
-                this.getListManager();
-                this.getListMember();
-            },
-            async getProflie() {
-                try {
-                    var response = await myTeamApi.findMyTeamsProfile(this.pTeamSeq);
-                    const {data} = response;
-                    this.profile = data;
-                } catch(e) {
-                    console.log(e);
-                }
-            },
-            async getListManager() {
-                try {
-                    var response = await myTeamApi.searchManagers(this.pTeamSeq);
-                    const {data} = response;
-                    this.managerList = data;
-                } catch(e) {
-                    console.log(e);
-                }
-            },
-            async getListMember() {
-                try {
-                    this.memberList = {};
-                    var response = await myTeamApi.searchMembers(this.pTeamSeq, this.pagination.page-1);
-                    const { data } = response;
-                    this.memberList = data;
-                    this.pagination.teamCount = this.memberList[0].pagerDTO.totalCount;
-                    this.pagination.totPagerNo = Math.ceil(this.pagination.teamCount / 3);
-                    console.log(this.pagination);
-                } catch (e) {
-                    console.log(e);
-                }
-            },
-            clickAddTeamMember() {
-                const teamSeq = this.pTeamSeq
-                this.$router.push({
-                    name : "MemberManagePage",
-                    params : { pTeamSeq : teamSeq },
-                })
-            },
-            handlePage() {
-                this.getListMember();
-            }
-        },
-        mounted () { 
-            this.onLoad();
-        }, 
-    }
+import MyTeamProfileModal from "@/views/myTeam/modal/MyTeamProfileModal.vue";
+
+export default {
+  //data: {} // Component끼리 data를 공유하면 안되므로 다음과 같이 사용하면 안됨.
+  data: () => {
+    return {
+      profile: {},
+      managerList: [],
+      memberList: [],
+      teamInfo: {},
+      dialog: false,
+      teamProfile: false,
+      pagination: {
+        page: 1,
+        teamCount: 0,
+        totPagerNo: 1,
+      },
+    };
+  },
+  props: {
+    pTeamSeq: {
+      type: Number,
+      required: true,
+    },
+  },
+  components: {
+    MyProfile,
+    MyManager,
+    MyMember,
+    // eslint-disable-next-line
+    MyTeamInfoModal,
+    MyTeamProfileModal,
+  },
+  methods: {
+    onLoad() {
+      this.getProflie();
+      this.getListManager();
+      this.getListMember();
+    },
+    async getProflie() {
+      try {
+        var response = await myTeamApi.findMyTeamsProfile(this.pTeamSeq);
+        const { data } = response;
+        this.profile = data;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async getListManager() {
+      try {
+        var response = await myTeamApi.searchManagers(this.pTeamSeq);
+        const { data } = response;
+        this.managerList = data;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async getListMember() {
+      try {
+        const response = await myTeamApi.searchMembers(
+          this.pTeamSeq,
+          this.pagination.page - 1
+        );
+        const { data } = response;
+        this.memberList = data;
+        this.pagination.teamCount = this.memberList[0].pagerDTO.totalCount;
+        this.pagination.totPagerNo = Math.ceil(this.pagination.teamCount / 3);
+        console.log(this.pagination);
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    clickAddTeamMember() {
+      const teamSeq = this.pTeamSeq;
+      this.$router.push({
+        name: "MemberManagePage",
+        params: { pTeamSeq: teamSeq },
+      });
+    },
+    handlePage() {
+      this.getListMember();
+    },
+    isManager() {
+      return authUtil.isManager(this.pTeamSeq);
+    },
+    isLeader() {
+      return authUtil.isLeader(this.pTeamSeq);
+    },
+  },
+  mounted() {
+    this.onLoad();
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
