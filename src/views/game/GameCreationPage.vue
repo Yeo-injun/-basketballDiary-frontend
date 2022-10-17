869fdff1bfd91f1e380115b1cd1866b492097509
Devy <67055084+Yeo-injun@users.noreply.github.com>
2022-10-17 23:50:10 +0900

    [Feat] 게임생성 화면, 게임참가팀선택 화면 vue파일 생성

---
+++ src/views/game/GameCreationPage.vue
@@ -0,0 +1,20 @@
+/** * 화면ID : G001P01 * 화면명 : 게임생성 화면 * 생성일시 : 2022.10.17 * 작성자
+: 여인준 * 작업일지 * 2022.10.17(월) 여인준 : 최초생성 */
+
+<template>
+  <v-container>
+    <h1>경기 정보</h1>
+    <v-button>다음</v-button>
+    경기일자 경기시간 주소 경기장명
+  </v-container>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
