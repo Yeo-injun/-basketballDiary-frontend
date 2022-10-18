c9d22ce8eba27a8c05d47a2e2b5db854dcdbdd28
Devy <67055084+Yeo-injun@users.noreply.github.com>
2022-10-19 00:27:49 +0900

    [Feat] 게임참가팀 선택 화면 퍼블리싱 및 게임생성 화면 수정

---
+++ src/components/game/GameOpponentSearchComp.vue
@@ -0,0 +1,58 @@
+<template>
+	<v-container>
+		<h2>상대팀 검색</h2>
+		<v-text-field label="경기">경기일자</v-text-field>
+		<v-text-field label="팀명" />
+		<v-text-field label="팀장이름" />
+		<v-btn>검색</v-btn>
+
+		<v-data-table
+			v-model="selected"
+			:headers="headers"
+			:items="opponents"
+			:single-select="true"
+			item-key="teamSeq"
+			show-select
+			class="elevation-1"
+		/>
+	</v-container>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				headers: [
+					{ text: '팀ID', value: 'teamSeq' },
+					{ text: '팀명', value: 'teamName' },
+					{ text: '팀장이름', value: 'leaderName' },
+					{ text: '설립일자', value: 'foundationYmd' },
+					{ text: '활동지역', value: 'hometown' },
+				],
+				opponents: [
+					// TODO  임시데이터
+					{
+						teamSeq: 1,
+						teamName: '바이러스',
+						leaderName: '코로나',
+						foundationYmd: '20221019',
+						hometown: '서울특별시',
+					},
+					{
+						teamSeq: 2,
+						teamName: '바이러스',
+						leaderName: '코로나',
+						foundationYmd: '20221019',
+						hometown: '서울특별시',
+					},
+				],
+				selected: [],
+			};
+		},
+		setup() {
+			return {};
+		},
+	};
+</script>
+
+<style lang="scss" scoped></style>
