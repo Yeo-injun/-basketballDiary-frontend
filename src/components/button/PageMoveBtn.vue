1d38d93b6a28070dadc0aded61f719a94fe25184
Devy <67055084+Yeo-injun@users.noreply.github.com>
2022-10-20 00:29:29 +0900

    [Feat] G001P01 경기일자 공통DatePicker 적용 / G001P01 G001P02 '다음'버튼 공통화 적용

---
+++ src/components/button/PageMoveBtn.vue
@@ -0,0 +1,34 @@
+<template>
+	<!-- TODO 버튼 위치 잡기 -->
+	<v-container>
+		<v-btn :to="{ name: pRouteCompName }">{{ btnName }}</v-btn>
+	</v-container>
+</template>
+
+<script>
+	export default {
+		props: {
+			pRouteCompName: String,
+			pBtnName: String,
+		},
+		data() {
+			return {
+				btnName: this.getBtnName(),
+			};
+		},
+		methods: {
+			getBtnName() {
+				if (
+					this.pBtnName == null ||
+					this.pBtnName == undefined ||
+					this.pBtnName == ''
+				) {
+					return '다음';
+				}
+				return this.pBtnName;
+			},
+		},
+	};
+</script>
+
+<style></style>
--- src/views/game/GameCreationPage.vue
+++ src/views/game/GameCreationPage.vue
@@ -2,10 +2,15 @@
 : 여인준 * 작업일지 * 2022.10.17(월) 여인준 : 최초생성 */
 
 <template>
+	<!-- props를 내려줄때 Tips
+	내려주는 props가 변수에 담겨있을 때는 :props명으로
+	내려주는 props가 값일때는 props명으로 작성
+	-->
 	<v-container>
 		<h2>경기 정보</h2>
-		<v-btn :to="{ name: 'GameJoinTeamSelectionPage' }">다음</v-btn>
-		<v-text-field label="경기">경기일자</v-text-field>
+		<PageMoveBtn pRouteCompName="GameJoinTeamSelectionPage" />
+
+		<CustomDatePickerComp p-label-name="경기일자" :p-init-value="currentYmd" />
 		<v-text-field label="경기시작시간" /> ~
 		<v-text-field label="경기종료시간" />
 		<v-text-field label="주소" />
@@ -16,18 +21,30 @@
 
 <script>
 	import AddressOpenApi from '@/common/address/AddressOpenApi.js';
+	import CustomDatePickerComp from '@/components/common/CustomDatePickerComp.vue';
+	import PageMoveBtn from '@/components/button/PageMoveBtn.vue';
+
+	import DateUtil from '@/common/DateUtil.js';
 
 	export default {
-		setup() {
-			return {};
+		components: {
+			CustomDatePickerComp,
+			PageMoveBtn,
 		},
-
+		data() {
+			return {
+				currentYmd: DateUtil.getCurrentYmd(),
+			};
+		},
 		methods: {
 			searchAddress() {
 				AddressOpenApi.open(function (data) {
 					console.log('===============');
 					console.log(data);
 				});
+			},
+			getCurrentYmd() {
+				return DateUtil.getCurrentYmd();
 			},
 		},
 	};
