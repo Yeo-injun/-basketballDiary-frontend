3fafe64afbb6e203c60956ac5a51857f78dc200d
Devy <67055084+Yeo-injun@users.noreply.github.com>
2022-10-31 23:41:56 +0900

    [Feat] API053 게임생성API 기본 세팅 완료. 세부 처리 구현 예정

--- src/views/game/GameCreationPage.vue
+++ src/views/game/GameCreationPage.vue
@@ -8,18 +8,31 @@
 	-->
 	<v-container>
 		<h2>경기 정보</h2>
-		<PageMoveBtn pRouteCompName="GameJoinTeamSelectionPage" />
+		<PageMoveBtn :pRouteCompName="this.routeCompName" />
+		<v-btn @click="createGame()">게임생성(임시)</v-btn>
 
-		<CustomDatePickerComp p-label-name="경기일자" :p-init-value="currentYmd" />
-		<v-text-field label="경기시작시간" /> ~
-		<v-text-field label="경기종료시간" />
-		<v-text-field label="주소" />
-		<v-btn @click="searchAddress()">주소검색</v-btn>
-		<v-text-field label="경기장명" />
+		<CustomDatePickerComp
+			p-label-name="경기일자"
+			:p-init-value="currentYmd"
+			@pickup-date="setGameYmd"
+		/>
+		<!-- 경기시작시간 및 종료시간 셀렉트박스로 만드는거 검토 -->
+		<v-text-field
+			label="경기시작시간"
+			v-model="gameCreationDTO.gameStartTime"
+		/>
+		~
+		<v-text-field label="경기종료시간" v-model="gameCreationDTO.gameEndTime" />
+
+		<v-text-field label="주소" v-model="gameCreationDTO.gamePlaceAddress" />
+		<v-btn @click="popupAddressSearch()">주소검색</v-btn>
+		<v-text-field label="경기장명" v-model="gameCreationDTO.gamePlaceName" />
 	</v-container>
 </template>
 
 <script>
+	import gameApi from '@/api/GameAPI.js';
+
 	import AddressOpenApi from '@/common/address/AddressOpenApi.js';
 	import CustomDatePickerComp from '@/components/common/CustomDatePickerComp.vue';
 	import PageMoveBtn from '@/components/button/PageMoveBtn.vue';
@@ -33,11 +46,24 @@
 		},
 		data() {
 			return {
+				routeCompName: 'GameJoinTeamSelectionPage',
 				currentYmd: DateUtil.getCurrentYmd(),
+				// TODO 파라미터값 정리 / rule부여해서 필수값 체크
+				gameCreationDTO: {
+					teamSeq: '1',
+					creatorTeamMemberSeq: '1',
+					gameYmd: '20221021',
+					gameStartTime: '1000',
+					gameEndTime: '1300',
+					gamePlaceAddress: '경기도 시흥시 배곧',
+					gamePlaceName: '빈대떡과찜',
+					sidoCode: '11',
+					sigunguCode: '110',
+				},
 			};
 		},
 		methods: {
-			searchAddress() {
+			popupAddressSearch() {
 				AddressOpenApi.open(function (data) {
 					console.log('===============');
 					console.log(data);
@@ -45,6 +71,14 @@
 			},
 			getCurrentYmd() {
 				return DateUtil.getCurrentYmd();
+			},
+			setGameYmd(ymd) {
+				this.gameCreationDTO.gameYmd = DateUtil.Format.toString(ymd);
+			},
+			createGame() {
+				const params = this.gameCreationDTO;
+				console.log(params);
+				gameApi.createGame(params);
 			},
 		},
 	};
