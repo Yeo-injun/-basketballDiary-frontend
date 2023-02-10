b080ec0b5f65a19b337d224ebcc62cace1dfbd9b
Devy <67055084+Yeo-injun@users.noreply.github.com>
2023-02-10 23:32:56 +0900

    [Feat] API 요청 HTTP 메소드 및 URL console 찍기

--- src/views/game/recordDetail/modal/tab/TeamMemberSearchTab.vue
+++ src/views/game/recordDetail/modal/tab/TeamMemberSearchTab.vue
@@ -1,6 +1,11 @@
 <template>
 	<div>
 		<div>팀원검색</div>
+		<!-- 검색창 -->
+		<v-text-field label="이름" v-model="playerName" />
+		<v-btn class="mb-2 mr-2" width="100" @click="searchTeamMember">
+			검색
+		</v-btn>
 		<PlayerDataTable />
 	</div>
 </template>
@@ -11,6 +16,17 @@
 	export default {
 		components: {
 			PlayerDataTable,
+		},
+		data() {
+			return {
+				playerName: '',
+			};
+		},
+		methods: {
+			async searchTeamMember() {
+				console.log(this.playerName);
+				alert('dd');
+			},
 		},
 	};
 </script>
