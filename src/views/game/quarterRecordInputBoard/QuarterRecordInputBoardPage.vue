62d7f81fc5a07994f8daabc11352ff1a33426d83
Devy <67055084+Yeo-injun@users.noreply.github.com>
2023-03-02 01:55:13 +0900

    [Feat] 쿼터기록입력 화면 - 입력버튼으로 선수 stat반영 완료 (입력 모드, 취소 모드 모두 구현 완료) 각 stat버튼 구현 예정

--- src/views/game/quarterRecordInputBoard/QuarterRecordInputBoardPage.vue
+++ src/views/game/quarterRecordInputBoard/QuarterRecordInputBoardPage.vue
@@ -21,10 +21,15 @@
 		<div v-if="this.isGetGameEntryLoadOk">
 			<HomeTeamRecordInputBoardComp
 				v-if="this.isHomeTeamInputMode"
+				:pHomeAwayCode="this.homeCode"
 				:pEntry="this.homeTeamEntry"
 				@get-clicked-record-info="mergeInputRecordStat"
 			/>
-			<AwayTeamRecordInputBoardComp v-else :pEntry="this.awayTeamEntry" />
+			<AwayTeamRecordInputBoardComp
+				v-else
+				:pHomeAwayCode="this.awayCode"
+				:pEntry="this.awayTeamEntry"
+			/>
 		</div>
 		// 하단 버튼 - 저장 / 쿼터삭제
 	</div>
@@ -34,6 +39,7 @@
 	import GameAPI from '@/api/GameAPI.js';
 
 	import { HomeAwayCode } from '@/const/code/GameCode.js';
+	import StatType from '@/const/StatType.js';
 
 	import GameQuarterInfoComp from '@/views/game/quarterRecordInputBoard/components/GameQuarterInfoComp.vue';
 	import HomeTeamTitle from '@/components/game/gameJoinTeam/TeamTitleComp.vue';
@@ -59,13 +65,17 @@
 					homeAwayCode: '',
 					homeAwayCodeName: '',
 				},
+				homeCode: HomeAwayCode.HOME_TEAM,
 				homeTeamEntry: [],
+				homeTeamRecords: [],
 				awayTeamTitleInfo: {
 					teamName: '',
 					homeAwayCode: '',
 					homeAwayCodeName: '',
 				},
+				awayCode: HomeAwayCode.AWAY_TEAM,
 				awayTeamEntry: [],
+				awayTeamRecords: [],
 			};
 		},
 		methods: {
@@ -102,9 +112,48 @@
 			},
 			mergeInputRecordStat(record) {
 				console.log(record);
-				// home팀이냐, away팀이냐 구분하고
-				// 해당되는 팀의 엔트리를 순회하여 해당 선수의 기록 변경
-				// TODO record에서 homeAwayCode속성 추가
+				const isHomeTeamRecords = HomeAwayCode.HOME_TEAM == record.homeAwayCode;
+				if (isHomeTeamRecords) {
+					// TODO 팀의 stat도 같이 올라가야함!! 추가 구현 예정
+					this.homeTeamRecords.push(record);
+
+					// 입력값을 받은 선수 filter
+					const targetPlayer = this.homeTeamEntry.filter(function (player) {
+						return player.gameJoinPlayerSeq == record.gameJoinPlayerSeq;
+					})[0];
+
+					// 스탯 유형에 따라 count
+					const statType = record.statType;
+
+					// 입력모드 체크
+					if (record.mode == 'add') {
+						switch (statType) {
+							case StatType.TWO_POINT:
+								targetPlayer.twoPoint++;
+								targetPlayer.tryTwoPoint++;
+								targetPlayer.totalScore += 2;
+								return;
+							case StatType.TRY_TWO_POINT:
+								targetPlayer.tryTwoPoint++;
+								// TODO 파울갯수 임시 테스트
+								targetPlayer.foul++;
+								return;
+						}
+					}
+
+					switch (statType) {
+						case StatType.TWO_POINT:
+							--targetPlayer.twoPoint;
+							--targetPlayer.tryTwoPoint;
+							targetPlayer.totalScore -= 2;
+							return;
+						case StatType.TRY_TWO_POINT:
+							--targetPlayer.tryTwoPoint;
+							return;
+					}
+				}
+
+				// TODO 어웨이팀 제어 구현 >> 객체와 메소드를 활용하여 중복 소스 제거 필요
 			},
 		},
 		mounted() {
