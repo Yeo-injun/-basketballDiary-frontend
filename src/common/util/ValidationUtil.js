ed9a213470d7dc72b054d01c3b67e2bfb17f7ea5
Devy <67055084+Yeo-injun@users.noreply.github.com>
2023-05-18 23:58:56 +0900

    [Feat] 경기참가선수관리 Modal - 비회원 게스트 등록 input 검증 로직 추가

--- src/common/util/ValidationUtil.js
+++ src/common/util/ValidationUtil.js
@@ -21,4 +21,21 @@ export default {
 		}
 		return val;
 	},
+	// 입력값 검증
+	input: {
+		checkMaxLength(value, restrictions) {
+			const targetVal = value || '';
+			const maxLen = restrictions?.maxLength || 5;
+			const restrictionMsg =
+				restrictions?.message || `최대 입력 가능 길이는 ${maxLen} 입니다.`;
+			return targetVal.length <= maxLen || restrictionMsg;
+		},
+		checkEmailPattern(value, restrictions) {
+			const restrictionMsg = restrictions?.message || `이메일 형식이 아닙니다.`;
+
+			const emailPattern =
+				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			return emailPattern.test(value) || restrictionMsg;
+		},
+	},
 };
