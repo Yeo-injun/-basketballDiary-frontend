4ace96e41ea48e39cb51831767089f0797f18226
Devy <67055084+Yeo-injun@users.noreply.github.com>
2023-07-02 20:20:47 +0900

    [Refactor] 게임기록상세조회 화면 - Page파일 외에 다른 컴포넌트들은 components폴더에서관리

--- src/views/game/recordDetail/GameRecordDetailPage.vue
+++ src/views/game/recordDetail/GameRecordDetailPage.vue
@@ -28,10 +28,11 @@
 <script>
 	import { GameRecordStateCode } from '@/const/code/GameCode.js';
 
-	import GameInfoComp from '@/views/game/recordDetail/GameInfoComp.vue';
-	import GameJoinTeamsInfoComp from '@/views/game/recordDetail/GameJoinTeamsInfoComp.vue';
-	import GameJoinPlayersInfoComp from '@/views/game/recordDetail/GameJoinPlayersInfoComp.vue';
-	import GameQuartersComp from '@/views/game/recordDetail/GameQuartersComp.vue';
+	import GameInfoComp from '@/views/game/recordDetail/components/GameInfoComp.vue';
+	import GameJoinTeamsInfoComp from '@/views/game/recordDetail/components/GameJoinTeamsInfoComp.vue';
+	import GameJoinPlayersInfoComp from '@/views/game/recordDetail/components/GameJoinPlayersInfoComp.vue';
+	import GameQuartersComp from '@/views/game/recordDetail/components/GameQuartersComp.vue';
+
 	import GameConfirmBtn from '@/views/game/recordDetail/button/GameConfirmBtn.vue';
 	import GameDeletionBtn from '@/components/game/button/GameDeletionBtn.vue';
 
@@ -48,7 +49,7 @@
 		data() {
 			const query = this.$route.query;
 			return {
-				gameSeq: query.gameSeq,
+				gameSeq: Number(query.gameSeq),
 				gameRecordState: query.gameRecordState,
 				teamSeq: query.teamSeq,
 				teamName: query.teamName,
