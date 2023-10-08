e0d1dc33345af59f6b2b4a44493656e04986205c
Devy <67055084+Yeo-injun@users.noreply.github.com>
2023-10-08 23:52:06 +0900

    [Chore] 자바스크립트 이벤트 연구

---
+++ src/components/EventTestComp.vue
@@ -0,0 +1,62 @@
+<template>
+	<v-btn
+		@click.right="onClick"
+		@contextmenu="onRightButtonClick"
+		@touchstart="onTouchStart"
+		@touchend="onTouchEnd"
+		@touchmove="onTouchMove"
+	>
+		이벤트 테스트 버튼
+	</v-btn>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				lastTapTime: 0,
+				timeSinceLastTap: 0,
+			};
+		},
+		methods: {
+			onRightButtonClick(e) {
+				console.log(e);
+				console.log(
+					'============================== RigthButton Click===================='
+				);
+				e.preventDefault();
+			},
+			onTouchStart() {
+				const currentTime = new Date().getTime();
+				this.timeSinceLastTap = currentTime - this.lastTapTime;
+				if (this.timeSinceLastTap < 300) {
+					console.log('DobleTab');
+					// console.log(this.lastTapTime);
+				}
+				// else {
+				// 	console.log('Tab');
+				// 	// console.log(this.lastTapTime);
+				// }
+
+				this.lastTapTime = currentTime;
+			},
+			onTouchEnd() {
+				if (this.timeSinceLastTap < 300) {
+					// console.log('DobleTab');
+					// console.log(this.lastTapTime);
+				} else {
+					console.log('TouchEnd');
+				} // console.log(this.lastTapTime);
+			},
+			onTouchMove(e) {
+				console.log('MOVE');
+				console.log(e);
+			},
+			onClick() {
+				console.log('CLICK');
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scoped></style>
