4056862913b914e25c581abf6daefe8660f18a72
Yeo-injun <youth.com11@gmail.com>
2023-12-12 00:33:48 +0900

    [Fix] ImageInput 공통 컴포넌트 이벤트 발생시 imageFile 속성이 null로 넘어오는 오류 해결

--- src/components/input/FrameImageInput.vue
+++ src/components/input/FrameImageInput.vue
@@ -1,16 +1,23 @@
 <template>
     <v-file-input
+    ref="imageInput"
         show-size
-        :label="this.pLabel || '수정할 프로필 사진 업로드'"
+        :label="this.pLabel || '사진 업로드'"
         accept="image/*"
         :model-value="this.imageFile"
         :rules="[ this.checkMaxFileSize ]"
+        @click:clear="onImageClear"
     />
 </template>
 
 <script>
     import ValidationUtil from '@/common/util/ValidationUtil.js';
 	
+    const EventName = {
+        EXCEED_MAX_SIZE : 'exceed-max-size',
+        SELECT_VALID_IMAGE : 'select-valid-image',
+        CLEAR_INPUT : "clear-input",
+    } 
     export default {
         props : {
             pLabel : String,
@@ -33,13 +40,24 @@
                 const MAX_BYTES = ValidationUtil.isNotNull( this.pMaxBytes ) 
                                 ? this.pMaxBytes : 1 * 1024 * 1024 * 3; // 3mb
                 if ( MAX_BYTES < imageFile.size ) {
-                    this.imageFile = null;
-                    this.$emit('exceed-max-size', `업로드 최대 크기 ( ${MAX_BYTES} )를 초과하였습니다.`);
+                    this.$emit( EventName.EXCEED_MAX_SIZE, {
+                        imageFile : null,
+                        errorMessage : `업로드 최대 크기 ( ${MAX_BYTES} )를 초과하였습니다.`, 
+                    });
                     return false;
                 }
                 // 유효할 경우에만 사진 파일을 emit함
-                this.$emit('select-valid-image', this.imageFile );
+                this.$emit( EventName.SELECT_VALID_IMAGE, {
+                    imageFile : imageFile,
+                    errorMessage : "", 
+                });
                 return true;
+            },
+            onImageClear() {
+                this.$emit( EventName.CLEAR_INPUT, {
+                    imageFile : null,
+                    errorMessage : "", 
+                });
             },
         },
     }
