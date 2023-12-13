8be28b3d8fe4a54f1c009491960841980933076b
Yeo-injun <youth.com11@gmail.com>
2023-12-14 01:16:32 +0900

    [Feat] BytesUtil 데이터 단위 변환시 소수점 첫번쨰자리까지 처리

--- src/common/util/BytesUtil.js
+++ src/common/util/BytesUtil.js
@@ -1,16 +1,20 @@
 export default {
     toKBs( bytes ) {
-        return bytes / UNIT_BYTES;
+        return Decimals.round( bytes / UNIT_BYTES );
     },
     toMBs( bytes ) {
-        return this.toKBs( bytes ) / UNIT_BYTES;
+        return Decimals.round( this.toKBs( bytes ) / UNIT_BYTES );
     },
     toGBs( bytes ) {
-        return this.toMBs( bytes ) / UNIT_BYTES;
+        return Decimals.round( this.toMBs( bytes ) / UNIT_BYTES );
     },
     toTBs( bytes ) {
-        return this.toTBs( bytes ) / UNIT_BYTES;
+        return Decimals.round( this.toTBs( bytes ) / UNIT_BYTES );
     },
+    /**
+     * Byte 사이즈를 알맞은 데이터 크기의 단위로 변환
+     * cf. 변환시 소수점 1번째까지 보여줌
+     */
     withUnit( bytes ) {
         const unitNames = Object.values( UnitName );
         let unitIdx = 0;
@@ -21,7 +25,22 @@ export default {
             dataSize = dataSize / UNIT_BYTES;
             unitSize = unitSize * UNIT_BYTES;
         }
-        return dataSize + unitNames[ unitIdx ]; 
+        return Decimals.round( dataSize ) + unitNames[ unitIdx ]; 
+    },
+}
+
+/**
+ * 소수점 처리 객체
+ */
+const Decimals = {
+    /**
+     * TODO toFixed가 반올림인지 버림인지 확인 필요
+     * @param { Number } num  소수점 처리하고자 하는 숫자 
+     * @param { Number } digits 보여주고자 하는 소수점자릿수
+     * @return 
+     */
+    round( num, digits ) {
+        return Number( num ).toFixed( digits ? digits : 1 );
     },
 }
 
