1e23f837c14669412b40427729717942004c47cc
Yeo-injun <youth.com11@gmail.com>
2024-01-01 22:47:50 +0900

    [Chore] 사용자 Agent Type 분기처리 테스트

--- src/views/test/ComponentTestPage.vue
+++ src/views/test/ComponentTestPage.vue
@@ -1,24 +1,27 @@
 <template>
-	<div v-if="this.env == 'local'">
+	<div>
 		사용자 브라우저 : {{ this.agentType }}
-		<EventTestComp />
-		<TestSlot :pList="list" :pPagination="pagination" @click-page="onClick">
-			<!-- 하위컴포넌트에서 slot에 할당한 데이터를 상위컴포넌트에서는 data라는 변수명으로 사용한다는 문법 -->
-			<!-- 상위컴포넌트에서 정의한 변수명 다음으로 하위 컴포넌트에서 정의한 변수명을 사용해야 해당 데이터에 접근가능 -->
-			<template v-slot:itemSlot="data">
-				<TestBtn :pBtnName="data.item.name" @do-add="onClick" />
-			</template>
-			<!-- 목록이 존재하지 않을 경우 보여줄 컴포넌트 -->
-			<template v-slot:itemEmptySlot> 검색결과가 없습니다. </template>
-		</TestSlot>
-		<StatInput />
-		<v-text-field
-				label="주소"
-				v-model="dataSize"
-				@change="onChangeByteUtilTest"
-							/>
+		<div v-if="this.env == 'local'">
+			사용자 브라우저 : {{ this.agentType }}
+			<EventTestComp />
+			<TestSlot :pList="list" :pPagination="pagination" @click-page="onClick">
+				<!-- 하위컴포넌트에서 slot에 할당한 데이터를 상위컴포넌트에서는 data라는 변수명으로 사용한다는 문법 -->
+				<!-- 상위컴포넌트에서 정의한 변수명 다음으로 하위 컴포넌트에서 정의한 변수명을 사용해야 해당 데이터에 접근가능 -->
+				<template v-slot:itemSlot="data">
+					<TestBtn :pBtnName="data.item.name" @do-add="onClick" />
+				</template>
+				<!-- 목록이 존재하지 않을 경우 보여줄 컴포넌트 -->
+				<template v-slot:itemEmptySlot> 검색결과가 없습니다. </template>
+			</TestSlot>
+			<StatInput />
+			<v-text-field
+					label="주소"
+					v-model="dataSize"
+					@change="onChangeByteUtilTest"
+								/>
 
-		BytesUtil 테스트 : {{ this.dataSize }}
+			BytesUtil 테스트 : {{ this.dataSize }}
+		</div>
 	</div>
 </template>
 
