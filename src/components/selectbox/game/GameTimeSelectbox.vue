c5f772b59ed928e271b351b21a547ba7407e7e73
Yeo-injun <youth.com11@gmail.com>
2024-01-23 00:17:45 +0900

    [Feat] TimeSelectbox 공통컴포넌트를 활용하여 경기시간 및 쿼터시간 selectbox 컴포넌트 대체

--- src/components/selectbox/game/GameTimeSelectbox.vue
+++ src/components/selectbox/game/GameTimeSelectbox.vue
@@ -1,50 +1,54 @@
 <template>
 	<v-row>
 		<v-col>
-			<MinuteSelectbox pLabelName="경기시간-분"
+			<HourSelectbox :pLabelName="this.hourLabelName"
+				pUnitType="hour" 
+				:pInitVal="this.hour"
+				@select-value="selectValue" 
+			/>			
+		</v-col>
+		<v-col>
+			<MinuteSelectbox :pLabelName="this.minuteLabelName"
 				pUnitType="min"
 				:pInitVal="this.minute"
 				@select-value="selectValue" 
 			/>
-		</v-col>
-		<v-col>
-			<SecondSelectbox pLabelName="경기시간-초"
-				pUnitType="sec" 
-				:pInitVal="this.second"
-				@select-value="selectValue" 
-			/>			
 		</v-col>
 	</v-row>
 </template>
 
 <script>
 	import MinuteSelectbox from '@/components/selectbox/TimeSelectbox.vue';
-	import SecondSelectbox from '@/components/selectbox/TimeSelectbox.vue';
+	import HourSelectbox from '@/components/selectbox/TimeSelectbox.vue';
 
 	import ValidationUtil from '@/common/util/ValidationUtil.js';
 
 	export default {
 		components : {
 			MinuteSelectbox	,
-			SecondSelectbox	,
+			HourSelectbox	,
 		},
 		props: {
-			pInitVal: String,
+			pInitVal	: String,
+			pLabelName 	: String,
 		},
 		data() {
-			const validInitVal = ValidationUtil.isNotNull( this.pInitVal ) && this.pInitVal.length == 4;
+			const labelNamePrefix 	= ValidationUtil.isNotNull( this.pLabelName ) ? this.pLabelName : '경기시간';
+			const validInitVal 		= ValidationUtil.isNotNull( this.pInitVal ) && this.pInitVal.length == 4;
 			return {
-				minute : validInitVal ? this.pInitVal.substr( 0, 2 ) : '0',
-				second : validInitVal ? this.pInitVal.substr( 2, 4 ) : '0',
+				hourLabelName 	: `${labelNamePrefix}-시`,
+				minuteLabelName : `${labelNamePrefix}-분`,
+				hour 	: validInitVal ? this.pInitVal.substring( 2, 4 ) : '0',
+				minute 	: validInitVal ? this.pInitVal.substring( 0, 2 ) : '0',
 			};
 		},
 		methods: {
 			selectValue( data ) {
 				switch( data.unitType ) {
-				case 'min' : this.minute = String( data.value ).padStart(2, '0'); break;
-				case 'sec' : this.second = String( data.value ).padStart(2, '0'); break;
+				case 'hour' : this.hour 	= String( data.value ).padStart(2, '0'); break;
+				case 'min' 	: this.minute 	= String( data.value ).padStart(2, '0'); break;
 				}
-				this.$emit('select-value', this.minute + this.second );
+				this.$emit('select-value', this.hour + this.minute );
 			},
 		},
 	};
