73cc7ff737a951f67dbc0fe33b45a214facff943
Yeo-injun <youth.com11@gmail.com>
2024-07-28 02:42:10 +0900

    [Feat] AuthStateManager의 사용자 권한정보 초기화 메소드 분리

--- src/common/state/AuthStateManager.js
+++ src/common/state/AuthStateManager.js
@@ -16,13 +16,30 @@ export default {
 		},
 	},
 	mutations: {
-		processLogin(authInfo) {
-			store.authUserInfo = authInfo;
-			store.isLogin = true;
+		// 새로고침시 권한 정보를 업데이트 하기 위한 메소드
+		init( authInfo ) {
+			store.authUserInfo 	= buildUserAuth( authInfo );
+			store.isLogin 		= authInfo.loginState;
 		},
+		// 로그인시 권한정보 세팅
+		processLogin(authInfo) {
+			store.authUserInfo 	= buildUserAuth( authInfo );
+			store.isLogin 		= true;
+		},
+		// 로그아웃시 권한정보 날리기
 		processLogout() {
 			store.authUserInfo = {};
 			store.isLogin = false;
 		},
 	},
 };
+
+
+function buildUserAuth( authInfo ) {
+	return {
+		userSeq		: authInfo.userSeq,
+		userId		: authInfo.userId,
+		authTeams	: authInfo.authTeams,
+		authGames	: authInfo.authGames,
+	}
+}
