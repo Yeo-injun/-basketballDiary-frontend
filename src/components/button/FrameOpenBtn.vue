d2f7bfa4922cbc79d245022f2f39cfcb73d54100
Yeo-injun <youth.com11@gmail.com>
2024-10-13 21:11:31 +0900

    [Feat] 소속팀 팀원 목록 조회 화면 - 프로필 수정 버튼 위치 변경 (프로필 영역으로 위치 변경)

--- src/components/button/FrameOpenBtn.vue
+++ src/components/button/FrameOpenBtn.vue
@@ -1,11 +1,16 @@
 <template>
-	<v-btn block color="secondary" @click="doOpen">{{ this.pBtnName }}</v-btn>
+	<v-btn :block="pStyleBlock" color="secondary" @click="doOpen">{{ this.pBtnName }}</v-btn>
 </template>
 
 <script>
 	export default {
 		props: {
 			pBtnName: String,
+			pStyleBlock : {
+				default() {
+					return true;
+				},
+			}
 		},
 		data() {
 			return {
