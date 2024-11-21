dfe2043e5fb17c43c38632f6f79b59a46f5dbd53
Yeo-injun <youth.com11@gmail.com>
2024-11-21 23:10:07 +0900

    [ Feat / View ] 소속팀 경기목록 조회 화면 - 경기기록상태에 따라 보여지는 정보 제어 ( 경기생성 상태인 경우 팀정보와 쿼터 정보는 노출시키지 않음

--- src/components/game/TeamScoreComp.vue
+++ src/components/game/TeamScoreComp.vue
@@ -6,19 +6,18 @@
 					<v-row no-gutters align-content="center" justify="center">
 						<!-- 파울기록Comp -->
 						<div v-if="this.isHomeTeam()">파울</div>
-						<!-- 점수Comp -->
-						<div v-else>{{ this.pTeamScoreInfo.gameTotalScore }}</div>
+						<div v-if="this.isAwayTeam()">{{ this.intlData.gameTotalScore }}</div>
 					</v-row>
 				</v-col>
 				<!-- HOME/AWAY여부  -->
 				<v-col cols="12" sm="4">
-					<v-row no-gutters justify="center">{{ this.pTeamScoreInfo.homeAwayCodeName }}</v-row>
-					<v-row no-gutters justify="center">{{ this.pTeamScoreInfo.teamName }}</v-row>
+					<v-row no-gutters justify="center">{{ this.intlData.homeAwayCodeName }}</v-row>
+					<v-row no-gutters justify="center">{{ this.intlData.teamName }}</v-row>
 				</v-col>
 				<v-col cols="12" sm="4" align-self="center">
 					<v-row no-gutters justify="center">
-						<div v-if="this.isHomeTeam()">{{ this.pTeamScoreInfo.gameTotalScore }}</div>
-						<div v-else>파울</div>
+						<div v-if="this.isHomeTeam()">{{ this.intlData.gameTotalScore }}</div>
+						<div v-if="this.isAwayTeam()">파울</div>
 					</v-row>
 				</v-col>
 			</v-row>
@@ -27,17 +26,26 @@
 </template>
 
 <script>
+	import { HomeAwayCode } from '@/const/code/GameCode';
 	export default {
+		// props의 각 속성 default()는 해당 속성이 props로 넘어오지 않을때만 실행됨 ( null이 넘어오면 default()는 실행되지 않음 )
 		props: {
-			pTeamScoreInfo: Object,
+			pTeamScoreInfo: {
+				type	: Object,
+			},
+		},
+		computed: {
+			intlData() {
+				return this.pTeamScoreInfo || {};
+			},
 		},
 		methods: {
 			isHomeTeam() {
-				if ( '01' == this.pTeamScoreInfo.homeAwayCode ) {
-					return true;
-				}
-				return false;
+				return HomeAwayCode.HOME_TEAM == this.intlData.homeAwayCode;
 			},
+			isAwayTeam() {
+				return HomeAwayCode.AWAY_TEAM == this.intlData.homeAwayCode;
+			}
 		},
 	};
 </script>
