62f3e7360b77be1772c4eb11e683746d99e49fc8
Yeo-injun <youth.com11@gmail.com>
2024-12-15 19:08:43 +0900

    [Feat / View&API ] 소속팀 상세화면 - 팀원Tab에 팀 탈퇴 기능 추가

--- src/views/myTeam/detail/components/member/TeamProfileComp.vue
+++ src/views/myTeam/detail/components/member/TeamProfileComp.vue
@@ -14,18 +14,25 @@
 	>
 		<template v-slot:action-slot >
 			<!-- 필요한 제어 버튼 구현 영역 -->
-			<ProfileUpdateBtn
-				:pStyleBlock="false"
-				@do-open="openProfileUpdateModal()"
-				pBtnName="프로필 수정"
-			/>
-			<ProfileUpdateModal
-				:pIsActivated="isActivatedProfileModal"
-				:pTeamSeq="pTeamProfile.teamSeq"
-				:pBackNumber="pTeamProfile.backNumber"
-				:pImageUrl="pTeamProfile.profileImageUrl"
-				@modal-close="closeProfileUpdateModal()"
-			/>
+			<div class="button-container" >
+				<ProfileDeleteBtn
+					pBtnName="팀탈퇴"
+					:pStyleBlock="false"
+					@do-delete="deleteTeamProfile()"
+				/>
+				<ProfileUpdateBtn
+					pBtnName="프로필 수정"
+					:pStyleBlock="false"
+					@do-open="openProfileUpdateModal()"
+				/>
+				<ProfileUpdateModal
+					:pIsActivated="isActivatedProfileModal"
+					:pTeamSeq="pTeamProfile.teamSeq"
+					:pBackNumber="pTeamProfile.backNumber"
+					:pImageUrl="pTeamProfile.profileImageUrl"
+					@modal-close="closeProfileUpdateModal()"
+				/>
+			</div>
 		</template>
 	</MemberInfoComp>
 </template>
@@ -33,22 +40,30 @@
 <script>
 
 	/** Backend API */
+	import MyTeamAPI from '@/api/MyTeamAPI';
 	/** CODE */
 	/** Utils */
+	import AuthManager from '@/common/auth/AuthManager';
 	/** Components */
-	import ProfileUpdateModal from '@/views/myTeam/detail/tab/modal/MyTeamProfileUpdateModal.vue';
+	import ProfileDeleteBtn from '@/components/button/FrameDeleteBtn.vue';
 	import ProfileUpdateBtn from '@/components/button/FrameOpenBtn.vue';
+	import ProfileUpdateModal from '@/views/myTeam/detail/tab/modal/MyTeamProfileUpdateModal.vue';
 
 	import MemberInfoComp from '@/views/myTeam/detail/components/member/FrameMemberInfoComp.vue';
 
 	export default {
 		components: {
+			ProfileDeleteBtn,
 			ProfileUpdateModal,
 			ProfileUpdateBtn,
 			MemberInfoComp,
 		},
 		props: {
 			pTeamProfile: Object,
+			pTeamSeq : {
+				required 	: true,
+				type 		: Number,
+			},
 		},
 		data() {
 			return {
@@ -56,6 +71,14 @@
 			}
 		},
 		methods: {
+			async deleteTeamProfile() {
+				if ( !confirm("팀 탈퇴를 하시겠습니까?") ) {
+					return;
+				}
+				await MyTeamAPI.withdrawTeam({ teamSeq : this.pTeamSeq });
+				AuthManager.init();					// 사용자 세션의 팀권한 상태 동기화
+				this.$router.push( "/myTeams" );	// 소속팀 목록으로 이동
+			},
 			/**-----------------------------
 			 * Modal 제어
 			 **-----------------------------*/
@@ -69,4 +92,12 @@
 	};
 </script>
 
-<style></style>
+<style>
+	.button-container {
+		display	: flex; /* 버튼들을 옆으로 정렬 */
+		gap		: 10px; /* 버튼 사이 간격 설정 */
+	}
+	.button-container .v-btn {
+		width: 120px;
+	}
+</style>
